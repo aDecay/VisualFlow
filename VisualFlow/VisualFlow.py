b55e16bf957e9cb279ffa0177025d17d083aa1ff
aDecay <younghyeon55@naver.com>
2018-02-22 15:39:00 +0900

    commit test

--- VisualFlow/VisualFlow.py
+++ VisualFlow/VisualFlow.py
@@ -3,60 +3,12 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QWidget
 from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QAction, qApp
 
-
-class Notepad(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.text = QTextEdit(self)
-        self.clr_btn = QPushButton('Clear')
-        self.sav_btn = QPushButton('Save')
-        self.opn_btn = QPushButton('Open')
-
-        self.init_ui()
-
-    def init_ui(self):
-        v_layout = QVBoxLayout()
-        h_layout = QHBoxLayout()
-
-        h_layout.addWidget(self.clr_btn)
-        h_layout.addWidget(self.sav_btn)
-        h_layout.addWidget(self.opn_btn)
-
-        v_layout.addWidget(self.text)
-        v_layout.addLayout(h_layout)
-
-        self.sav_btn.clicked.connect(self.save_text)
-        self.clr_btn.clicked.connect(self.clear_text)
-        self.opn_btn.clicked.connect(self.open_text)
-
-        self.setLayout(v_layout)
-        self.setWindowTitle('PyQt5 TextEdit')
-
-        self.show()
-
-    def save_text(self):
-        filename = QFileDialog.getSaveFileName(self, 'Save File', os.getenv('HOME'))
-        with open(filename[0], 'w') as f:
-            my_text = self.text.toPlainText()
-            f.write(my_text)
-
-    def open_text(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
-        with open(filename[0], 'r') as f:
-            file_text = f.read()
-            self.text.setText(file_text)
-
-    def clear_text(self):
-        self.text.clear()
-
-
-class Writer(QMainWindow):
+class VisualFlow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.form_widget = Notepad()
-        self.setCentralWidget(self.form_widget)
+        #self.form_widget =
+        #self.setCentralWidget(self.form_widget)
 
         self.init_ui()
 
@@ -117,7 +69,6 @@ class Writer(QMainWindow):
         elif signal == '&Save':
             self.form_widget.save_text()
 
-
 app = QApplication(sys.argv)
-writer = Writer()
+visualFlow = VisualFlow()
 sys.exit(app.exec_())
