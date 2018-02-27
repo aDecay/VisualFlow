6e2181fa2509249a7282091a69550287ba67b4cb
SoroaWang <7700nsw@naver.com>
2018-02-27 21:31:00 +0900

    Hello Hello!

--- VisualFlow/ObjectsWidget.py
+++ VisualFlow/ObjectsWidget.py
@@ -3,8 +3,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class ObjectsWidget(QWidget):
-    def __init__(self, baseWidget):
+    def __init__(self,baseWidget):
         super(ObjectsWidget, self).__init__()
+        self.baseWidget = baseWidget
+
+        self.Pb = QPushButton("누르면 옆으로 길어져요!")
+        self.Pb.clicked.connect(self.btn_click)
 
         self.imageLabel = QLabel()
         self.imageLabel2 = QLabel()
@@ -14,6 +18,10 @@ class ObjectsWidget(QWidget):
         self.imageLabel2.setPixmap(QPixmap.fromImage(self.image2))
 
         self.v_box = QVBoxLayout(self)
+        self.v_box.addWidget(self.Pb)
         self.v_box.addWidget(self.imageLabel)
         self.v_box.addWidget(self.imageLabel2)
         self.v_box.setAlignment(Qt.AlignTop)
+
+    def btn_click(self):
+        self.baseWidget.tab1.insertColumn(0)
